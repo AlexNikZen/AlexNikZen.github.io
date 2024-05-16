6f14bfc73537fd84a47d8270c362207f3ebc1957
Alex Nikitin <alex.nikitin@zendesk.com>
2024-05-16 14:57:50 +0100

    commit1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello there!
+        </p>
+        <div>Simple react app on github pages</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
